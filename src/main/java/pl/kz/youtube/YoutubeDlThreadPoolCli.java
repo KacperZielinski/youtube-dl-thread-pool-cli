46d914df7df7fc18f9af412befcbc9a0c01d7632
Kacper Zielinski <kacper.zielinski@mailfence.com>
2021-03-03 20:00:13 +0100

    Added overall status to console

--- src/main/java/pl/kz/youtube/YoutubeDlThreadPoolCli.java
+++ src/main/java/pl/kz/youtube/YoutubeDlThreadPoolCli.java
@@ -3,6 +3,7 @@ package pl.kz.youtube;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class YoutubeDlThreadPoolCli {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
@@ -10,10 +11,14 @@ public class YoutubeDlThreadPoolCli {
         final Set<String> youtubeVideos = YoutubeFileTransformer.createYoutubeVideoUrlsSetFromFile(input.getFilePath());
 
         ForkJoinPool customThreadPool = new ForkJoinPool(input.getNumberOfInstances());
+
+        AtomicInteger taskLeft = new AtomicInteger(youtubeVideos.size());
         customThreadPool.submit(() -> youtubeVideos.parallelStream()
             .forEach(videoUrl -> {
                 YoutubeProcessHandler processHandler = new YoutubeProcessHandler();
                 processHandler.downloadVideoFromUrl(videoUrl);
+                taskLeft.getAndDecrement();
+                System.out.println("Overall status: " + taskLeft + "/" + youtubeVideos.size());
             })
         ).get();
     }
